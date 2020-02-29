cdc25642ff7b575ab7edba3a7d057c32fbb5c6ce
Amritesh <dmamritesh@gmail.com>
2020-03-01 00:51:21 +0530

    Complete UI Overhaul

--- lib/UI/newUI.dart
+++ lib/UI/newUI.dart
@@ -21,13 +21,11 @@ class ExtendedHome extends StatelessWidget {
                 elevation:0,
                 backgroundColor: backgroundColor,
               ),
-              body: DrawerLayout()),
+              body: DrawerLayout()
+            ),
           ),
           appBar: AppBar(
-            
-            actions: <Widget>[
-              
-            ],
+            actions: <Widget>[],
             backgroundColor: backgroundColor,
             bottom: TabBar(indicatorColor: Colors.purpleAccent,labelColor: Colors.purpleAccent.shade100,unselectedLabelColor: Colors.white,
               tabs: [
@@ -36,8 +34,7 @@ class ExtendedHome extends StatelessWidget {
               ],
             ),
             centerTitle: true,
-            title: Text('EXTENDED',style: TextStyle(fontSize: 22,letterSpacing:7),),
-            
+            title: Text('EXTENDED',style: TextStyle(fontSize: 22,letterSpacing:7),)
           ),
           body: TabBarView(
             children: [
@@ -45,22 +42,188 @@ class ExtendedHome extends StatelessWidget {
               SecondScreen(),
             ],
           ),
-
         ),
       ),
     );
   }
 }
 
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  const StaggeredTile.count(8, 4),
+  const StaggeredTile.count(4, 4),
+  const StaggeredTile.count(4, 6),
+  const StaggeredTile.count(4, 6),
+  const StaggeredTile.count(4, 4),
+];
+
 class FirstScreen extends StatelessWidget {
-  final seconfTabColor = Color(0xFF1d1d1d);
+  final firstTabColor = Color(0xFF1d1d1d);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: seconfTabColor,
-      body: Center(
-        child: Text('Tab 2 Layout'),
-      ),
+      backgroundColor: firstTabColor,
+      body: Padding(
+        padding: EdgeInsets.only(top: 12.0),
+        child: StaggeredGridView.count(
+          crossAxisCount: 8,
+          staggeredTiles: _staggeredTiles,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          padding: EdgeInsets.all(20.0),
+          children: <Widget>[
+            Container(
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Sun.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(22),
+                      onTap: (){},
+                      child: Center(
+                        child: Text("Event 1",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ),
+            ),
+            Container(
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Sun.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(22),
+                      onTap: (){},
+                      child: Center(
+                        child: Text("Event 2",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ),
+            ),
+            Container(
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Sun.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(22),
+                      onTap: (){},
+                      child: Center(
+                        child: Text("Event 3",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ),
+            ),
+            Container(
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Sun.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(22),
+                      onTap: (){},
+                      child: Center(
+                        child: Text("Event 4",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ),
+            ),
+            Container(
+              child: Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Sun.jpg'),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(22),
+                      onTap: (){},
+                      child: Center(
+                        child: Text("Event 5",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
