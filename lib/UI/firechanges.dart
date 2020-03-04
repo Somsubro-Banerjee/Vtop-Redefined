292f3c3d2b5d47bb262691981b126966a4194f04
Somsubro Banerjee <somsubrob@gmail.com>
2020-03-04 10:50:20 +0530

    more stuff added though firebase

--- lib/UI/firechanges.dart
+++ lib/UI/firechanges.dart
@@ -283,7 +283,7 @@ class SecondScreen extends StatelessWidget {
         stream: Firestore.instance.collection('posts').snapshots(),
         builder: (context, snapshot){
           if(!snapshot.hasData){
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
           else{
             return ListView.builder(
@@ -292,45 +292,51 @@ class SecondScreen extends StatelessWidget {
               itemCount: snapshot.data.documents.length,
               itemBuilder: (context, index){
                 DocumentSnapshot docsSnap = snapshot.data.documents[index];
-                return  Container(
+                return  AnimatedContainer(
+                  curve: Curves.fastOutSlowIn,
+                  duration:Duration(seconds:2) ,
+                  // width:  ? 200.0 : 100,
+                    child: Container(
+                    padding: EdgeInsets.all(10),
               child: Ink(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1.0,
-                    color: Colors.grey.shade700,
-                    style: BorderStyle.solid
-                  ),
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                    image: NetworkImage('${docsSnap['image']}'),
-                    fit: BoxFit.cover
-                  )
-                ),
-                child: ClipRRect(borderRadius: BorderRadius.circular(20),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(22),
-                      onTap: (){},
-                      child: Stack(
-                        children: <Widget>[
-                          Padding(padding: EdgeInsets.only(top: 120, left: 15),
-                            child: Text('${docsSnap['text']}',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white
-                              ),
-                            ),
-                          )
-                        ]
-                      )
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: Colors.grey.shade700,
+                      style: BorderStyle.solid
                     ),
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: NetworkImage('${docsSnap['image']}'),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                  child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 2.5,sigmaY: 2.5),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(22),
+                        onTap: (){},
+                        child: Stack(
+                          children: <Widget>[
+                            Padding(padding: EdgeInsets.only(bottom: 40, top: 150, left: 20),
+                              child: Text('${docsSnap['text']}',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                                ),
+                              ),
+                            )
+                          ]
+                        )
+                      ),
+                    )
                   )
-                )
               ),
-            );
+            ),
+                );
                 
               },
             );
