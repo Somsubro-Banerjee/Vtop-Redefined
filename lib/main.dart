3b7ddd50cbfc7d49221279edb87b41180188f971
lwplan123 <somsubrob@gmail.com>
2020-04-09 22:38:04 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
- import 'package:vtop/Authentication/Login.dart';
- import './UI/firechanges.dart';
- import 'Authentication/forgotPass.dart';
- import 'Authentication/Login.dart';
- import 'Authentication/Signup.dart';
+import 'package:vtop/Authentication/Login.dart';
+import 'Authentication/forgotPass.dart';
+import 'Authentication/Login.dart';
+import 'Authentication/Signup.dart';
 // import './UI/SplashScreen.dart';
 void main() {
   runApp(
